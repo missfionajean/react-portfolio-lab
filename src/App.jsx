d6c3f505414cda32d8f39ceb72b53e0a6d19c5c0
missfionajean <missfionajean@gmail.com>
2024-12-03 11:46:36 -0500

    refactored featured project into a variable object for easy updating

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import { renderToNodeStream } from "react-dom/server";
 function App() {
 	const allRepos =
 		"https://github.com/missfionajean?tab=repositories&q=&type=&language=&sort=";
+	const featured = {
+		name: "Swoon",
+		blurb: "Swoon is an in-browser game that uses JavaScript DOM manipulation and extensive CSS to run a full press-your-luck dating sim on a single webpage! Icons made by me using Canva Pro. All other assets rendered using CSS.",
+		image: "images/swoon.png",
+		alt: "Swoon Screenshot",
+		link: "https://missfionajean.github.io/browser-based-game/",
+	};
 	const mySkills = [
 		{
 			name: "JavaScript",
@@ -40,61 +47,56 @@ function App() {
 
 	return (
 		<>
-      <div id="page">
-        <img src="images/face.png" alt="Picture of Fiona" width="200" />
-        <h1>Fiona Jean</h1>
-        <h2>Full-Stack Developer</h2>
-        <h2>About</h2>
-        <p>
-          Hello! I am a full-stack developer, currently enrolled in
-          General Assembly's Software Engineering Bootcamp. Check out what I've been working on so far:
-        </p>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h2>Featured Project</h2>
-          <p>
-            Swoon is an in-browser game that uses JavaScript DOM
-            manipulation and extensive CSS to run a full press-your-luck dating sim on a single webpage! Icons made by me using Canva Pro. All other assets rendered using CSS.
-          </p>
-          <img
-            src="images/swoon.png"
-            alt="Swoon Screenshot"
-            width="400"
-          />
-          <a
-            href="https://missfionajean.github.io/browser-based-game/"
-            style={{
-              fontSize: 24,
-              margin: 15,
-              padding: "10px 20px",
-              border: "1px solid white",
-              borderRadius: 5,
-              backgroundColor: "#393939",
-              color: "white"
-            }}
-          >
-            Play Swoon Here!
-          </a>
-        </div>
-        <h2>Skills</h2>
-        <ul style={{ listStyleType: "none", padding: 0 }}>
-          {mySkills.map((skill) => (
-            <li style={{ display: "flex", flexDirection: "column" }}>
-              <h3>{skill.name}</h3>
-              <p>{skill.blurb}</p>
-              <a href={skill.link}>
-                View {skill.name}-Specific Repos
-              </a>
-              <a href={allRepos}>View All Git Repos</a>
-            </li>
-          ))}
-        </ul>
-      </div>
+			<div id="page">
+				<img src="images/face.png" alt="Picture of Fiona" width="200" />
+				<h1>Fiona Jean</h1>
+				<h2>Full-Stack Developer</h2>
+				<h2>About</h2>
+				<p>
+					Hello! I am a full-stack developer, currently enrolled in
+					General Assembly's Software Engineering Bootcamp. Check out what I've been working on so far:
+				</p>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<h2>Featured Project</h2>
+					<p>{featured.blurb}</p>
+					<img src={featured.image} alt={featured.alt} width="450" />
+					<a
+						href={featured.link}
+						style={{
+							fontSize: 24,
+							margin: 15,
+							padding: "10px 20px",
+							border: "1px solid white",
+							borderRadius: 5,
+							backgroundColor: "#393939",
+							color: "white",
+						}}
+					>
+						Play {featured.name} Here!
+					</a>
+				</div>
+				<h2>Skills</h2>
+				<ul style={{ listStyleType: "none", padding: 0 }}>
+					{mySkills.map((skill) => (
+						<li
+							style={{ display: "flex", flexDirection: "column" }}
+						>
+							<h3>{skill.name}</h3>
+							<p>{skill.blurb}</p>
+							<a href={skill.link}>
+								View {skill.name}-Specific Repos
+							</a>
+							<a href={allRepos}>View All Git Repos</a>
+						</li>
+					))}
+				</ul>
+			</div>
 		</>
 	);
 }
